e1eaa342b7d83b96cb0f367c1f98db866a12a78e
ludoguenet <ludovicguenet@gmx.com>
2025-04-08 13:59:32 +0200

    fix: update sitemap generation to use correct document fetching method and URL structure

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,16 +1,15 @@
 import { SitemapStream, streamToPromise } from 'sitemap'
-import { serverQueryContent } from '#content/server'
 
 export default defineEventHandler(async (event) => {
-  // Fetch all documents
-  const docs = await serverQueryContent(event).find()
+  // Fetch all contents
+  const docs = await queryCollection(event, 'content').order('order', 'ASC').all()
   const sitemap = new SitemapStream({
-    hostname: 'https://apprendre-laravel-12.net',
+    hostname: 'http://apprendre-laravel-12.net',
   })
 
   for (const doc of docs) {
     sitemap.write({
-      url: doc._path,
+      url: doc.path,
       changefreq: 'monthly',
     })
   }
