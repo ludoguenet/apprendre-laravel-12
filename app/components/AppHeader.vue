2df985da5b87fbe471bb4626075cef89b86bbdbb
ludoguenet <ludovicguenet@gmx.com>
2025-04-09 12:17:19 +0200

    fix: update YouTube button to use NuxtLink for better routing

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -21,24 +21,19 @@
       </NuxtLink>
 
       <!-- Bouton YouTube -->
-      <a
-        href="https://www.youtube.com/channel/UC36hi0WMeiR8HpUy-A2s4vQ?sub_confirmation=1"
+      <NuxtLink
+        to="https://www.youtube.com/channel/UC36hi0WMeiR8HpUy-A2s4vQ?sub_confirmation=1"
+        external
         target="_blank"
         rel="noopener"
-        class="flex items-center space-x-2 px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded-full shadow transition"
+        class="inline-flex items-center gap-1.5 px-2.5 py-1.5 text-sm bg-red-600 hover:bg-red-700 text-white rounded-full shadow transition"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 24 24"
-          class="size-5"
-        >
-          <path
-            d="M23.5 6.5s-.2-1.6-.8-2.3c-.8-.9-1.7-.9-2.1-1C17.3 3 12 3 12 3h-.1s-5.3 0-8.6.2c-.4 0-1.3.1-2.1 1C.7 4.9.5 6.5.5 6.5S0 8.3 0 10.1v1.8c0 1.8.5 3.6.5 3.6s.2 1.6.8 2.3c.8.9 1.9.9 2.3 1C6.7 19 12 19 12 19s5.3 0 8.6-.2c.4 0 1.3-.1 2.1-1 .6-.7.8-2.3.8-2.3s.5-1.8.5-3.6v-1.8c0-1.8-.5-3.6-.5-3.6zM9.6 14.7v-5.4l5.2 2.7-5.2 2.7z"
-          />
-        </svg>
+        <UIcon
+          name="i-lucide-youtube"
+          class="size-4"
+        />
         <span>S’abonner</span>
-      </a>
+      </NuxtLink>
     </div>
   </header>
 </template>
