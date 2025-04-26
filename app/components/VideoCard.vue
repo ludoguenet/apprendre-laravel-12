006477c7eaf9164d51b34ff01928c7a0255f61a3
ludoguenet <ludovicguenet@gmx.com>
2025-04-26 16:27:57 +0200

    updated alert badge wording

--- app/components/VideoCard.vue
+++ app/components/VideoCard.vue
@@ -36,7 +36,7 @@
               name="i-lucide-badge-alert"
               class="text-white"
             />
-            Nouveau
+            chaud !
           </div>
           <div
             class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 bg-black/30 transition-opacity"
